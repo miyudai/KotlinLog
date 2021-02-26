821062fd3f65b14d9e425a2186f71a5cafdcb6c4
miyudai <miyudai4649@gmail.com>
2021-02-26 15:07:44 +0900

    課題訂正版

--- app/src/main/java/jp/techacademy/daichi/miyuki/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/daichi/miyuki/kotlinlog/Human.kt
@@ -3,6 +3,8 @@ import android.util.Log
 
 open class Human:Animal,Thinkable {
 
+    var hobby: String
+
     constructor(name: String, age: Int,hobby:String ): super(name, age,hobby) {
 
         this.name = name
